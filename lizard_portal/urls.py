91fd90cb01458231cb665419bc038811b51fa601
Gijs Nijholt <gijs.nijholt@gmail.com>
2011-10-11 10:12:42 +0200

    Deleting extjs from media/lizard_portal.

--- lizard_portal/urls.py
+++ lizard_portal/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import include
 from django.conf.urls.defaults import patterns
 from django.conf.urls.defaults import url
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
 from lizard_ui.urls import debugmode_urlpatterns
 
@@ -11,8 +12,10 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     (r'^admin/', include(admin.site.urls)),
-    # url(r'^something/',
-    #     direct.import.views.some_method,
-    #     name="name_it"),
+    url(r'^example_treedata.json$', 
+        direct_to_template,
+        {'template': 'example_treedata.json', 'mimetype': 'application/json'},
+        name="example-treedata"),    
+
     )
 urlpatterns += debugmode_urlpatterns()
