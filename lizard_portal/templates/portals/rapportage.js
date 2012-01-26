382ca786f255276669a6ce09cacb8728ce90b263
Bastiaan Roos <bastiaan.roos@nelen-schuurmans.nl>
2012-01-26 13:25:49 +0100

    removed alerts

--- lizard_portal/templates/portals/rapportage.js
+++ lizard_portal/templates/portals/rapportage.js
@@ -14,8 +14,6 @@
                 var me = this;
                 me.setLoading(true);
                 var cm = Ext.getCmp('portalWindow').header.context_manager
-                alert(cm.objects.krw_waterlichaam.object_id)
-                alert(cm.objects.aan_afvoergebied.object_id)
                 me.loader.load({url:'/reporting/',
                     params:{
                         krw_waterlichaam: cm.objects.krw_waterlichaam.object_id,
