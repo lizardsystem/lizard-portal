4e21309f880c3a464a0a2a32ccd2c8338967e785
Bastiaan Roos <bastiaan.roos@nelen-schuurmans.nl>
2012-02-26 20:48:18 +0100

    removed unused files

--- lizard_portal/media/lizard_portal/viewer.coffee
+++
@@ -1,4 +0,0 @@
-Ext.application
-    name: 'lizardViewer'
-    launch: ->
-        Ext.create 'Lizard.Window.Viewer'
--- lizard_portal/media/lizard_portal/viewer.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  Ext.application({
-    name: 'lizardViewer',
-    launch: function() {
-      return Ext.create('Lizard.Window.Viewer');
-    }
-  });
-}).call(this);
