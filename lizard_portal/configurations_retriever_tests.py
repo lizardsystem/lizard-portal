9610f070051e00c7e05741328ced308ac81639ef
Pieter Swinkels <swinkels.pieter@yahoo.com>
2012-04-11 12:03:10 +0200

    Moves all non-test code out of configurations_retriever_tests.py
    This commit references nens/vss%96

--- lizard_portal/configurations_retriever_tests.py
+++ lizard_portal/configurations_retriever_tests.py
@@ -5,20 +5,17 @@
 
 # Copyright (c) 2012 Nelen & Schuurmans.  GPL licensed, see LICENSE.rst.
 
-import re
-import os
-
 from UserList import UserList
 from unittest import TestCase
-from zipfile import ZipFile
 
-from dbfpy import dbf
 from mock import Mock
 
 from lizard_area.models import Area
 from lizard_portal.configurations_retriever import ConfigurationFactory
 from lizard_portal.configurations_retriever import ConfigurationsRetriever
-from lizard_portal.configurations_retriever import Database
+from lizard_portal.configurations_retriever import ConfigurationSpecRetriever
+from lizard_portal.configurations_retriever import ConfigurationStore
+from lizard_portal.configurations_retriever import ConfigurationTypeRetriever
 from lizard_portal.configurations_retriever import DescriptionParser
 from lizard_portal.configurations_retriever import ZipFileNameRetriever
 from lizard_portal.models import ConfigurationToValidate
@@ -331,96 +328,6 @@ def get_self_c():
     return global_s
 
 
-class ConfigurationStore(object):
-
-    def __init__(self):
-        self.db = Database()
-        self.retrieve_zip_names = ZipFileNameRetriever().retrieve
-        self.retrieve_config_type = ConfigurationTypeRetriever().retrieve
-        self.retrieve_config_specs = ConfigurationSpecRetriever().retrieve
-
-    def supply(self):
-        config = self.db.ConfigurationToValidate()
-        for zip_name in self.retrieve_zip_names():
-            dir_name = self.retrieve_destination_dir(zip_name)
-            config_type = self.retrieve_config_type(zip_name)
-            self.extract(zip_name, dir_name)
-            for config_spec in self.retrieve_config_specs(dir_name, config_type):
-                for key, value in config_spec.items():
-                    if key == 'area_code':
-                        config.area = self.db.areas.get(code=value)
-                    else:
-                        setattr(config, key, value)
-                config.file_path = dir_name
-                config.action = ConfigurationToValidate.KEEP
-                config.save()
-
-    def retrieve_zip_names(self):
-        """Retrieve the path to all the configuration zip files .
-
-        This method is not implemented here and should be set through
-        dependency injection.
-
-        """
-        assert False
-
-    def retrieve_destination_dir(self, zip_name):
-        return os.path.join(self.dbf_directory, zip_name[:-4])
-
-    def extract(self, zip_name, destination_dir):
-        """Extract the given zip file to the given destination dir."""
-        ZipFile(zip_name).extractall(destination_dir)
-
-    def retrieve_config_type(self, zip_name):
-        """Return the configuration type using the name of the zip file.
-
-        Parameters:
-          *zip_name*
-             path to the zip file with the configurations of a water manager
-
-        This method is not implemented here and should be set through
-        dependency injection.
-
-        """
-        assert False
-
-    def retrieve_config_specs(self, dir_name, config_type):
-        """Retrieve the list of configuration specifications.
-
-        A configuration specification is a dict that maps each attribute name
-        to its attribute value.
-
-        Parameters:
-          *dir_name*
-             directory with the configuration specs of a single water manager
-          *config_type*
-             string that specifies the type of the configuration
-
-        This method is not implemented here and should be set through
-        dependency injection.
-
-        """
-        assert False
-
-    @property
-    def dbf_directory(self):
-        return '/tmp'
-
-
-class ConfigurationTypeRetriever(object):
-
-    def __init__(self):
-        self.regex = re.compile('^([\w\d]*)_[a-zA-Z]*_\d{8}_\d{6}.zip')
-
-    def retrieve(self, zip_name):
-        """Return the configuration type using the name of the zip file."""
-        _, file_name = os.path.split(zip_name)
-        match = self.regex.search(file_name)
-        if match and len(match.groups()) == 1:
-            matched_string = match.group(1)
-            return matched_string.lower().replace('_', '')
-
-
 class ConfigurationTypeRetrieverTestSuite(TestCase):
 
     def test_a(self):
@@ -480,35 +387,6 @@ class ConfigurationStoreTestSuite(TestCase):
         args, kwargs = self.store.retrieve_config_specs.call_args
         self.assertEqual('/tmp/waterbalans_Waternet_04042012_081400', args[0])
         self.assertEqual('waterbalans', args[1])
-
-
-class ConfigurationSpecRetriever(object):
-
-    def retrieve(self, dir_name, config_type):
-        dbf_name = os.path.join(dir_name, 'aanafvoer_%s.dbf' % config_type)
-        config_specs = []
-        for area_code in self.retrieve_area_codes(dbf_name):
-            meta_info_name = os.path.join(dir_name, 'description.txt')
-            config_spec = self.retrieve_meta_info(meta_info_name)
-            config_spec['area_code'] = area_code
-            config_specs.append(config_spec)
-        return config_specs
-
-    def retrieve_area_codes(self, dbf_name):
-        """Return the list of area codes in the given dbf file."""
-        return [rec['GAFIDENT'] for rec in dbf.Dbf(dbf_name)]
-
-    def retrieve_meta_info(self, meta_info_name):
-        """Return the meta info specified in the given file.
-
-        This method returns the meta info as a dict of attribute name to
-        attribute value.
-
-        This method is not implemented here and should be set through
-        dependency injection.
-
-        """
-        assert False
 
 
 class ConfigurationSpecRetrieverTestSuite(TestCase):
