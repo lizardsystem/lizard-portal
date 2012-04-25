7260ca44fa3b6cfa825ae525eabf0aa3daf5df91
Pieter Swinkels <swinkels.pieter@yahoo.com>
2012-04-25 13:26:31 +0200

    Updates the config diff template to actually show field differences
    The data shown has been hard-coded.

--- lizard_portal/views.py
+++ lizard_portal/views.py
@@ -188,8 +188,18 @@ def view_config_diff(request, area_name,
                             template='lizard_portal/config_diff.html'):
     logger.debug('lizard_portal.views.view_config_diff')
     logger.debug('look for ConfigurationToValidate for Area with name: %s', area_name)
-    get_object_or_404(ConfigurationToValidate, area__name=area_name)
+    config = get_object_or_404(ConfigurationToValidate, area__name=area_name)
+    diff = {
+        '3201 (Stichtsch Ankeveensche Polder)': {
+            'DIEPTE': ('1.17', '1.18'),
+            'EXTINCTIE': ('0.5', '0.55'),
+            'MOERAS_OR': ('False', 'True'),
+            }
+        }
     return render_to_response(
         template,
-        {},
+        { 'name': config.area.name,
+          'type': config.config_type,
+          'diff': diff,
+        },
         context_instance=RequestContext(request))
